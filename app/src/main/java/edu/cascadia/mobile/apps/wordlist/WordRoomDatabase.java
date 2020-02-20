d3e649e5162e84ec857e4462a97114acf8878d43
Clay Suttner <csuttner@gmail.com>
2020-02-19 21:02:56 -0800

    Adds @Database notation to WordDatabase class to fix crashing error

--- app/src/main/java/edu/cascadia/mobile/apps/wordlist/WordRoomDatabase.java
+++ app/src/main/java/edu/cascadia/mobile/apps/wordlist/WordRoomDatabase.java
@@ -2,6 +2,7 @@ package edu.cascadia.mobile.apps.wordlist;
 
 import android.content.Context;
 
+import androidx.room.Database;
 import androidx.room.Room;
 import androidx.room.RoomDatabase;
 import androidx.sqlite.db.SupportSQLiteDatabase;
@@ -11,6 +12,7 @@ import java.util.ConcurrentModificationException;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
+@Database(entities = {Word.class}, version = 1, exportSchema = false)
 public abstract class WordRoomDatabase extends RoomDatabase {
 
     public abstract WordDao wordDao();
